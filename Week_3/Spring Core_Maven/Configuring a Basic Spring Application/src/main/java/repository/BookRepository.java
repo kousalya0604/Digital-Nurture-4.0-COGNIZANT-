ebfd66c9cf8b19b7962ea25168983495ab5c16b3
kousalya0604 <kousalya8362@gmail.com>
2025-07-05 09:31:02 +0530

    Add files via upload

---
+++ Week_3/Spring Core_Maven/Configuring a Basic Spring Application/src/main/java/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public BookRepository() {
+        System.out.println("This is BookRepository");
+    }
+
+    public void fetchBook() {
+        System.out.println("Fetching book from repository...");
+    }
+}
